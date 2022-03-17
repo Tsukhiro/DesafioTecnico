19493c6c08799c5e03981ca3022cd620211996c3
Wagner Vitor Novais <wagner_wolf@outlook.com>
2022-03-17 09:30:10 -0300

    First commit
    first commit of the project almost done

---
+++ script.js
@@ -0,0 +1,119 @@
+const select = document.getElementById('inputGroupSelect01')
+
+changeLocal = () => {
+    
+    const textLocal = document.getElementById('descricao')
+
+    if(select.value === 'Acre (AC)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE"
+    }
+
+    if(select.value === 'Alagoas (AL)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 2222"
+    }
+
+    if(select.value === 'Amapá (AP)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Amazonas (AM)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Bahia (BA)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Ceará (CE)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Distrito Federal (DF)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Espírito Santo (ES)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Goiás (GO)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Maranhão (MA)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Mato Grosso (MT)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Mato Grosso do Sul (MS)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Minas Gerais (MG)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Pará (PA)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Paraíba (PB)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+    
+    if(select.value === 'Paraná (PR)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Pernambuco (PE)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Piauí (PI)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Rio de Janeiro (RJ)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Rio Grande do Norte (RN)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Rio Grande do Sul (RS)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Rondônia (RO)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Roraima (RR)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Santa Catarina (SC)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'São Paulo (SP)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Sergipe (SE)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    if(select.value === 'Tocantins (TO)'){
+        descricao.innerHTML = "OLÁ ESSA É UMA DESCRIÇÃO DA CIDADE 3333"
+    }
+
+    
+}
+
+select.addEventListener('change', changeLocal)
+
